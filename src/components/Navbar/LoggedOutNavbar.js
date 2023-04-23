6055fc2d88e23e9a9efc4987bfd1342e61be1d73
Diego Cividini <cividini.diego.dev@gmail.com>
2023-04-23 17:48:01 +0200

    🤑 Crypto API (CoinCap.io) added in market page

--- src/components/Navbar/LoggedOutNavbar.js
+++ src/components/Navbar/LoggedOutNavbar.js
@@ -1,20 +1,22 @@
-import Link from 'next/link';
-import Image from 'next/image';
+import Link from "next/link";
+import Image from "next/image";
 
 const LoggedOutNavbar = () => {
   return (
-    <nav className='w-screen top-0 mx-auto font-semibold bg-white bg-opacity-50 sticky z-50 backdrop-blur-md flex flex-row justify-center'>
-      <div className='w-[75vw] my-3'>
-        <div className='flex flex-row justify-center items-center'>
-          <div className='w-1/3'>
-            <Image src='/logo.png' alt='logo' width={50} height={50} />
+    <nav className="w-screen top-0 mx-auto font-semibold bg-white bg-opacity-50 sticky z-50 backdrop-blur-md flex flex-row justify-center">
+      <div className="w-[75vw] my-3">
+        <div className="flex flex-row justify-center items-center">
+          <div className="w-1/3">
+            <Link href="/">
+              <Image src="/logo.png" alt="logo" width={50} height={50} />
+            </Link>
           </div>
-          <div className='w-1/3'>
-            <ul className='flex flex-row justify-around'>
-              <li className=''>
+          <div className="w-1/3">
+            <ul className="flex flex-row justify-around">
+              <li className="">
                 <Link
-                  href='/market'
-                  className='py-2 px-4 hover:bg-slate-300 hover:rounded-full hover:bg-opacity-25'
+                  href="/market"
+                  className="py-2 px-4 hover:bg-slate-300 hover:rounded-full hover:bg-opacity-25"
                 >
                   Market
                 </Link>
@@ -22,10 +24,10 @@ const LoggedOutNavbar = () => {
                   //? pagina API Binance -> listone di tutte le cripto con prezzo istantaneo
                 }
               </li>
-              <li className=''>
+              <li className="">
                 <Link
-                  href='/'
-                  className='py-2 px-4 hover:bg-slate-300 hover:rounded-full hover:bg-opacity-25'
+                  href="/"
+                  className="py-2 px-4 hover:bg-slate-300 hover:rounded-full hover:bg-opacity-25"
                 >
                   Forum
                 </Link>
@@ -33,10 +35,10 @@ const LoggedOutNavbar = () => {
                   //? reddit like forum
                 }
               </li>
-              <li className=''>
+              <li className="">
                 <Link
-                  href='/'
-                  className='py-2 px-4 hover:bg-slate-300 hover:rounded-full hover:bg-opacity-25'
+                  href="/"
+                  className="py-2 px-4 hover:bg-slate-300 hover:rounded-full hover:bg-opacity-25"
                 >
                   Support
                 </Link>
@@ -46,22 +48,22 @@ const LoggedOutNavbar = () => {
               </li>
             </ul>
           </div>
-          <div className='w-1/3'>
-            <ul className='flex flex-row justify-end space-x-4 font-normal p-2'>
+          <div className="w-1/3">
+            <ul className="flex flex-row justify-end space-x-4 font-normal p-2">
               <li>
                 <Link
-                  href='/'
-                  className='group text-black transition-all duration-200 ease-in-out'
+                  href="/"
+                  className="group text-black transition-all duration-200 ease-in-out"
                 >
-                  <span className='pb-1 bg-left-bottom bg-gradient-to-r from-pink-500 to-pink-500 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-200 ease-out'>
+                  <span className="pb-1 bg-left-bottom bg-gradient-to-r from-pink-500 to-pink-500 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-200 ease-out">
                     Login
                   </span>
                 </Link>
               </li>
               <li>
                 <Link
-                  href='/'
-                  className='text-white bg-pink-500 rounded-full py-2 px-4 hover:bg-pink-600'
+                  href="/"
+                  className="text-white bg-pink-500 rounded-full py-2 px-4 hover:bg-pink-600"
                 >
                   Sign Up
                 </Link>
--- src/pages/market/index.js
+++ src/pages/market/index.js
@@ -1,6 +1,64 @@
-const { Expletus_Sans } = require('next/font/google');
+import React, { useState, useEffect, Fragment } from "react";
+import LoggedOutNavbar from "@/components/Navbar/LoggedOutNavbar";
 
 const Market = () => {
-  return <div>dvwerqerv2e</div>;
+  const [cryptoData, setCryptoData] = useState([]);
+  useEffect(() => {
+    let requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+    const fetchMarketData = async () => {
+      const response = await fetch("https://api.coincap.io/v2/assets")
+        .then((response) => response.json())
+        .catch((error) => console.log("error", error));
+      setCryptoData(response.data);
+    };
+    fetchMarketData();
+  }, []);
+  return (
+    <>
+      <LoggedOutNavbar />
+      <div className="flex flex-col justify-center w-full items-center m-2">
+        <table className="bg-slate-300 rounded-xl p-4 text-center">
+          <thead className="text-center">
+            <tr className="text-center">
+              <th className="p-4">Name</th>
+              <th className="p-4">Symbol</th>
+
+              <th className="p-4">Price</th>
+              <th className="p-4">24h</th>
+              <th className="p-4">Market Cap</th>
+              <th className="p-4">Volume</th>
+              <th className="p-4">Last 7 Days</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cryptoData.map((cryptoData, index) => (
+              <tr key={index}>
+                <td className="p-4 capitalize">{cryptoData.id}</td>
+                <td className="p-4">{cryptoData.symbol}</td>
+                <td className="p-4">
+                  ${parseFloat(cryptoData.priceUsd).toFixed(2)}
+                </td>
+                <td className="p-4">
+                  ${parseFloat(cryptoData.changePercent24Hr).toFixed(2)}
+                </td>
+                <td className="p-4">
+                  ${parseFloat(cryptoData.marketCapUsd).toFixed(2)}
+                </td>
+                <td className="p-4">
+                  ${parseFloat(cryptoData.volumeUsd24Hr).toFixed(2)}
+                </td>
+                <td className="p-4">
+                  ${parseFloat(cryptoData.vwap24Hr).toFixed(2)}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 };
 export default Market;
