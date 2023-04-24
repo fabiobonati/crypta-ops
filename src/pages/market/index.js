0cf4a9a3da922b532a0fcec8cff859cfbfbad022
fabiobonati <bonatifabio.dev@gmail.com>
2023-04-24 20:44:06 +0200

    Market page responsive table

--- src/pages/market/index.js
+++ src/pages/market/index.js
@@ -24,65 +24,63 @@ const Market = () => {
   const itemsToShow = cryptoData.slice(startIndex, endIndex);
   return (
     <>
-      <div className='flex flex-col justify-center w-full mx-auto items-center m-6 p-2'>
-        <table className='bg-gray-50 rounded-xl p-4 text-center'>
-          <thead className='text-center'>
-            <tr className='text-center'>
-              <th className='p-4 text-left'>Name</th>
-              <th className='p-4'>Price</th>
-              <th className='p-4'>Trend 24h</th>
-              <th className='p-4'>Market Cap</th>
-              <th className='p-4'>Volume</th>
-              <th className='p-4'>Last 7 Days</th>
-            </tr>
-          </thead>
-          <tbody>
-            {itemsToShow.map((cryptoData, index) => (
-              <tr
-                key={index}
-                className='border-t-2 border-gray-200 hover:bg-gray-100'
-                data-href='/'
-              >
-                <td className='p-4 capitalize'>
-                  <div className='flex flex-row text-left'>
-                    <Image
-                      src={'/logo.png'}
-                      width={36}
-                      height={36}
-                      alt={cryptoData.id}
-                      className='mr-2'
-                    />
-                    <div>
-                      <p>{cryptoData.id}</p>
-                      <p className='text-gray-500'>{cryptoData.symbol}</p>
-                    </div>
-                  </div>
-                </td>
-                <td className='p-4'>
-                  ${parseFloat(cryptoData.priceUsd).toFixed(2)}
-                </td>
-                <td
-                  className={
-                    parseFloat(cryptoData.changePercent24Hr).toFixed(2) < 0
-                      ? 'text-red-500'
-                      : 'text-green-400'
-                  }
-                >
-                  {parseFloat(cryptoData.changePercent24Hr).toFixed(2)}%
-                </td>
-                <td className='p-4'>
-                  ${parseFloat(cryptoData.marketCapUsd).toFixed(2)}
-                </td>
-                <td className='p-4'>
-                  ${parseFloat(cryptoData.volumeUsd24Hr).toFixed(2)}
-                </td>
-                <td className='p-4'>
-                  ${parseFloat(cryptoData.vwap24Hr).toFixed(2)}
-                </td>
+      <div className='flex flex-col justify-center w-full container p-2 mx-auto items-center'>
+        <div className='border-2 rounded-lg border-gray-100'>
+          <table className='text-center mx-auto w-full'>
+            <thead className='hidden border-b-2 border-gray-200 sm:table-header-group'>
+              <tr className='text-center'>
+                <th className='p-4 text-left'>Name</th>
+                <th className='p-4'>Price</th>
+                <th className='p-4'>Trend 24h</th>
+                <th className='p-4'>Market Cap</th>
+                <th className='p-4'>Volume</th>
+                <th className='p-4 hidden md:table-cell'>Last 7 Days</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {itemsToShow.map((cryptoData, index) => (
+                <tr key={index} className=' hover:bg-gray-100' data-href='/'>
+                  <td className='p-4 capitalize'>
+                    <div className='flex flex-row text-left'>
+                      <Image
+                        src={'/logo.png'}
+                        width={36}
+                        height={36}
+                        alt={cryptoData.id}
+                        className='mr-2'
+                      />
+                      <div>
+                        <p className='font-medium'>{cryptoData.id}</p>
+                        <p className='text-gray-500'>{cryptoData.symbol}</p>
+                      </div>
+                    </div>
+                  </td>
+                  <td className='p-4'>
+                    ${parseFloat(cryptoData.priceUsd).toFixed(2)}
+                  </td>
+                  <td
+                    className={
+                      parseFloat(cryptoData.changePercent24Hr).toFixed(2) < 0
+                        ? 'text-red-500 p-4'
+                        : 'text-green-400 p-4'
+                    }
+                  >
+                    {parseFloat(cryptoData.changePercent24Hr).toFixed(2)}%
+                  </td>
+                  <td className='p-4 hidden sm:table-cell'>
+                    ${parseFloat(cryptoData.marketCapUsd).toFixed(2)}
+                  </td>
+                  <td className='p-4 hidden sm:table-cell'>
+                    ${parseFloat(cryptoData.volumeUsd24Hr).toFixed(2)}
+                  </td>
+                  <td className='p-4 hidden md:table-cell'>
+                    ${parseFloat(cryptoData.vwap24Hr).toFixed(2)}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
         <div className='flex justify-center mt-4'>
           {Array.from({ length: totalPages }, (_, i) => i + 1).map(
             (pageNumber) => (
