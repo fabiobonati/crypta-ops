ae46f793be4dec79007d5422640a5ba395c8db15
Diego Cividini <cividini.diego.dev@gmail.com>
2023-04-26 22:35:53 +0200

    crypto adaptive chart added in crypto page

--- src/pages/market/[id].js
+++ src/pages/market/[id].js
@@ -1,13 +1,72 @@
+import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
-
-const MarketPage = () => {
+import {
+  Chart as ChartS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler,
+} from 'chart.js';
+// 2. Register them
+ChartS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Title,
+  Tooltip,
+  Legend
+);
+import { Line } from 'react-chartjs-2';
+const MarketData = () => {
   const router = useRouter();
   const { id } = router.query;
-  return (
-    <div>
-      <h1>Page of {id}</h1>
-    </div>
-  );
+  const [cryptoData, setCryptoData] = useState([]);
+
+  const data = {
+    labels: cryptoData.map((data) =>
+      new Date(data.date).toLocaleDateString('it-IT')
+    ),
+    datasets: [
+      {
+        label: 'Price',
+        data: cryptoData.map((data) => data.priceUsd),
+        fill: true,
+        backgroundColor: 'rgba(75,192,192,0.2)',
+        borderColor: 'rgba(75,192,192,1)',
+      },
+    ],
+  };
+  const options = {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+  };
+  console.log(id);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(
+        `https://api.coincap.io/v2/assets/${id}/history?interval=m1`
+      );
+      const data = await res.json();
+      setCryptoData(data.data);
+    };
+    fetchData();
+  }, []);
+
+  return <Line data={data} width={100} height={40} options={options} />;
 };
 
-export default MarketPage;
+export default MarketData;
