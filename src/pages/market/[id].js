8457a3bb2c7f751ce278f15b9b01d7ebcb6ca196
Diego Cividini <cividini.diego.dev@gmail.com>
2023-04-28 13:17:44 +0200

    style to crypto page with fix on header and chart

--- src/pages/market/[id].js
+++ src/pages/market/[id].js
@@ -28,7 +28,8 @@ const MarketData = () => {
   const { id } = router.query;
   const [ID, setID] = useState('');
   const [cryptoData, setCryptoData] = useState([]);
-  const [selectedCrypto, setSelectedCrypto] = useState('default');
+  const [selectedCrypto, setSelectedCrypto] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     if (router.isReady) {
       // Code using query
@@ -48,13 +49,31 @@ const MarketData = () => {
     fetchData();
   }, [id]);
   useEffect(() => {
-    const fetchCryptoData = async () => {
-      const res = await fetch(`https://api.coincap.io/v2/assets/${ID}`);
-      const data = await res.json();
-      setSelectedCrypto(data.data);
-    };
+    let isMounted = true; // flag to track component mount state
+
+    async function fetchCryptoData() {
+      try {
+        const response = await fetch(`https://api.coincap.io/v2/assets/${ID}`);
+        const data = await response.json();
+        if (isMounted) {
+          setSelectedCrypto(data.data);
+          setIsLoading(false);
+        }
+      } catch (error) {
+        console.error(error);
+        if (isMounted) {
+          setSelectedCrypto({});
+        }
+      }
+    }
+
     fetchCryptoData();
+
+    return () => {
+      isMounted = false; // set flag to false on unmount
+    };
   }, [ID]);
+  const crypto = selectedCrypto;
   const data = {
     labels: cryptoData.map(
       (data) =>
@@ -65,8 +84,8 @@ const MarketData = () => {
         label: 'Price',
         data: cryptoData.map((data) => data.priceUsd),
         fill: true,
-        backgroundColor: 'rgba(255, 215, 0, 0.2)',
-        borderColor: 'rgba(255, 215, 0, 0.9)',
+        backgroundColor: 'rgba(233, 30, 99, 0.2)',
+        borderColor: 'rgba(233, 30, 99, 0.9)',
       },
     ],
   };
@@ -75,6 +94,11 @@ const MarketData = () => {
     elements: {
       point: {
         radius: 0,
+      },
+    },
+    plugins: {
+      legend: {
+        display: false,
       },
     },
     scales: {
@@ -88,19 +112,46 @@ const MarketData = () => {
     },
   };
 
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
   return (
     <div className="p-6">
-      <h1 className="text-2xl font-bold">{selectedCrypto.name}</h1>
-      <h1>{selectedCrypto.symbol}</h1>
+      <div className="flex flex-row justify-around">
+        <div className="flex flex-col gap-2">
+          <h1 className="text-2xl font-bold mt-4">{crypto.name}</h1>
+          <h2 className="mb-5">{crypto.symbol}</h2>
+        </div>
+        <div className="flex flex-col ml-4 gap-2">
+          <h1 className="text-2xl font-bold mt-4">
+            ${parseFloat(crypto.priceUsd).toFixed(2)}
+          </h1>
+          <h2 className="mb-5">
+            {crypto.changePercent24Hr > 0 ? (
+              <span className="text-green-500">
+                {parseFloat(crypto.changePercent24Hr).toFixed(2)}%
+              </span>
+            ) : (
+              <span className="text-red-500">
+                {parseFloat(crypto.changePercent24Hr).toFixed(2)}%
+              </span>
+            )}
+          </h2>
+        </div>
+        <div className="flex flex-col align-middle justify-center gap-2">
+          <button className="bg-pink-500 text-white p-4 rounded-full">
+            Buy now!
+          </button>
+        </div>
+      </div>
       <Line
         data={data}
         width={200}
-        height={100}
+        height={75}
         options={options}
         className="w-full h-full"
       />
     </div>
   );
 };
-
 export default MarketData;
