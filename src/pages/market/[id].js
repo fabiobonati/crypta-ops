e0c331874255fc72073adddb84735f6215d4af24
Diego Cividini <cividini.diego.dev@gmail.com>
2023-04-28 14:00:18 +0200

    👩🏻‍🎨 design crypto page

--- src/pages/market/[id].js
+++ src/pages/market/[id].js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useRouter } from 'next/router';
+import Image from 'next/image';
 import {
   Chart as ChartS,
   CategoryScale,
@@ -23,6 +24,49 @@ ChartS.register(
   Legend
 );
 import { Line } from 'react-chartjs-2';
+const symbolsArray = [
+  'steth',
+  'busd',
+  'shib',
+  'okb',
+  'cro',
+  'ldo',
+  'near',
+  'btcb',
+  'ftm',
+  'egld',
+  'usdp',
+  'flow',
+  'cfx',
+  'axs',
+  'rpl',
+  'hbar',
+  'rndr',
+  'klay',
+  'inj',
+  'mina',
+  'fxs',
+  'cspr',
+  'xec',
+  'gt',
+  'cake',
+  'ftt',
+  'woo',
+  'twt',
+  'xdc',
+  'rune',
+  'agix',
+  'kava',
+  'dydx',
+  'cvx',
+  'fei',
+  'rose',
+  'mask',
+  'dfi',
+  'audio',
+  'tfuel',
+];
+
 const MarketData = () => {
   const router = useRouter();
   const { id } = router.query;
@@ -30,6 +74,7 @@ const MarketData = () => {
   const [cryptoData, setCryptoData] = useState([]);
   const [selectedCrypto, setSelectedCrypto] = useState({});
   const [isLoading, setIsLoading] = useState(true);
+  const [selectedInterval, setSelectedInterval] = useState(1);
   useEffect(() => {
     if (router.isReady) {
       // Code using query
@@ -38,6 +83,16 @@ const MarketData = () => {
       setID(id);
     }
   }, [router.isReady, id]);
+
+  const fetchDataInterval = async (id, interval, buttonInterval) => {
+    const res = await fetch(
+      `https://api.coincap.io/v2/assets/${id}/history?interval=${interval}`
+    );
+    const data = await res.json();
+    setCryptoData(data.data);
+    setSelectedInterval(buttonInterval);
+  };
+
   useEffect(() => {
     const fetchData = async () => {
       const res = await fetch(
@@ -48,6 +103,7 @@ const MarketData = () => {
     };
     fetchData();
   }, [id]);
+
   useEffect(() => {
     let isMounted = true; // flag to track component mount state
 
@@ -66,14 +122,14 @@ const MarketData = () => {
         }
       }
     }
-
     fetchCryptoData();
-
     return () => {
       isMounted = false; // set flag to false on unmount
     };
   }, [ID]);
+
   const crypto = selectedCrypto;
+
   const data = {
     labels: cryptoData.map(
       (data) =>
@@ -89,6 +145,7 @@ const MarketData = () => {
       },
     ],
   };
+
   const options = {
     responsive: true,
     elements: {
@@ -115,12 +172,29 @@ const MarketData = () => {
   if (isLoading) {
     return <div>Loading...</div>;
   }
+
   return (
     <div className="p-6">
       <div className="flex flex-row justify-around">
-        <div className="flex flex-col gap-2">
-          <h1 className="text-2xl font-bold mt-4">{crypto.name}</h1>
-          <h2 className="mb-5">{crypto.symbol}</h2>
+        <div className="flex flex-row">
+          <div className="flex flex-col justify-center">
+            <Image
+              src={
+                !symbolsArray.includes(crypto.symbol.toLowerCase())
+                  ? `/color/${crypto.symbol.toLowerCase()}.svg`
+                  : `/color/generic.svg`
+              }
+              width={75}
+              height={57}
+              alt={cryptoData.id}
+              className="mr-2"
+            />
+          </div>
+
+          <div className="flex flex-col gap-2 ml-6">
+            <h1 className="text-2xl font-bold mt-4">{crypto.name}</h1>
+            <h2 className="mb-5">{crypto.symbol}</h2>
+          </div>
         </div>
         <div className="flex flex-col ml-4 gap-2">
           <h1 className="text-2xl font-bold mt-4">
@@ -139,7 +213,7 @@ const MarketData = () => {
           </h2>
         </div>
         <div className="flex flex-col align-middle justify-center gap-2">
-          <button className="bg-pink-500 text-white p-4 rounded-full">
+          <button className="bg-pink-500 hover:bg-pink-600 text-white p-4 rounded-full">
             Buy now!
           </button>
         </div>
@@ -151,6 +225,56 @@ const MarketData = () => {
         options={options}
         className="w-full h-full"
       />
+      <div className="flex flex-row justify-around gap-4 text-white mt-4">
+        <button
+          onClick={() => fetchDataInterval(id, 'm1', 1)}
+          className={`p-4 bg-pink-500 hover:bg-pink-600 rounded-full w-full h-full ${
+            selectedInterval === 1 ? 'bg-pink-600' : 'bg-pink-500'
+          }`}
+        >
+          1m
+        </button>
+        <button
+          onClick={() => fetchDataInterval(id, 'm5', 2)}
+          className={`p-4 bg-pink-500 hover:bg-pink-600 rounded-full w-full h-full ${
+            selectedInterval === 2 ? 'bg-pink-600' : 'bg-pink-500'
+          }`}
+        >
+          5m
+        </button>
+        <button
+          onClick={() => fetchDataInterval(id, 'm30', 3)}
+          className={`p-4 bg-pink-500 hover:bg-pink-600 rounded-full w-full h-full ${
+            selectedInterval === 3 ? 'bg-pink-600' : 'bg-pink-500'
+          }`}
+        >
+          30m
+        </button>
+        <button
+          onClick={() => fetchDataInterval(id, 'h1', 4)}
+          className={`p-4 bg-pink-500 hover:bg-pink-600 rounded-full w-full h-full ${
+            selectedInterval === 4 ? 'bg-pink-600' : 'bg-pink-500'
+          }`}
+        >
+          1h
+        </button>
+        <button
+          onClick={() => fetchDataInterval(id, 'h12', 5)}
+          className={`p-4 bg-pink-500 hover:bg-pink-600 rounded-full w-full h-full ${
+            selectedInterval === 5 ? 'bg-pink-600' : 'bg-pink-500'
+          }`}
+        >
+          12h
+        </button>
+        <button
+          onClick={() => fetchDataInterval(id, 'd1', 6)}
+          className={`p-4 bg-pink-500 hover:bg-pink-600 rounded-full w-full h-full ${
+            selectedInterval === 6 ? 'bg-pink-600' : 'bg-pink-500'
+          }`}
+        >
+          1d
+        </button>
+      </div>
     </div>
   );
 };
