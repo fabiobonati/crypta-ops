9796a4f83ddb400187d7cd35fb168e7c9198f701
Diego Cividini <cividini.diego.dev@gmail.com>
2023-05-04 09:15:23 +0200

    fix error (try 2)

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
 /** @type {import('next').NextConfig} */
+const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin');
 
 const nextConfig = {
   images: {
     domains: ['assets.coingecko.com', 'cryptoicons.org'],
   },
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+
+    return config;
+  },
   env: {
     DATABASE_URL: process.env.DATABASE_URL,
   },
