6c585f0fadfee0cb05c1cfb739a0e433d3d510f3
fabiobonati <bonatifabio.dev@gmail.com>
2023-05-22 08:14:22 +0200

    Started working on Dashboard Frontend

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect, use } from 'react';
 import { useRouter } from 'next/router';
 import { useSession } from 'next-auth/react';
 import { useForm } from 'react-hook-form';
+import Image from 'next/image';
+import { data } from 'autoprefixer';
 
 const Dashboard = () => {
   const session = useSession();
@@ -10,6 +12,53 @@ const Dashboard = () => {
   const [wallet, setWallet] = useState(null);
   const [userData, setUserData] = useState({});
   const [amount, setAmount] = useState();
+  const [transactions, setTransactions] = useState([]);
+  const [cryptoData, setCryptoData] = useState([]);
+  const [userCurrencies, setUserCurrencies] = useState([]);
+  const symbolsArray = [
+    'steth',
+    'busd',
+    'shib',
+    'okb',
+    'cro',
+    'ldo',
+    'near',
+    'btcb',
+    'ftm',
+    'egld',
+    'usdp',
+    'flow',
+    'cfx',
+    'axs',
+    'rpl',
+    'hbar',
+    'rndr',
+    'klay',
+    'inj',
+    'mina',
+    'fxs',
+    'cspr',
+    'xec',
+    'gt',
+    'cake',
+    'ftt',
+    'woo',
+    'twt',
+    'xdc',
+    'rune',
+    'agix',
+    'kava',
+    'dydx',
+    'cvx',
+    'fei',
+    'rose',
+    'mask',
+    'dfi',
+    'audio',
+    'tfuel',
+    'celo',
+  ];
+
   const { handleSubmit } = useForm();
 
   useEffect(() => {
@@ -29,27 +78,29 @@ const Dashboard = () => {
         .then((data) => {
           setUserData(data.user);
           setWallet(data.user.Wallet);
-          setIsLoading(false);
-        });
-      fetch('/api/transactions/', {
-        method: 'GET',
-      })
-        .then((res) => {
-          if (res.status === 401) {
-            router.push('/');
-          }
-          return res.json();
-        })
-        .then((data) => {
-          setAmount(data.balance);
+          fetch('/api/transactions/', {
+            method: 'GET',
+          })
+            .then((res) => {
+              if (res.status === 401) {
+                router.push('/');
+              }
+              return res.json();
+            })
+            .then((data) => {
+              setTransactions(data);
+              setUserCurrencies(Object.keys(data));
+              fetch('/api/wallet').th;
+              fetchMarketData();
+              setIsLoading(false);
+            });
         });
     }
-  }, [session, router]);
+  }, []);
 
   const onSubmit = async () => {
     try {
       const body = { userId: userData.id, startingAmount: amount };
-      console.log(body);
       const res = await fetch(`/api/wallet`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -64,6 +115,32 @@ const Dashboard = () => {
       console.error(error);
     }
   };
+
+  //call to COINCAP APIs to get data about crypto
+  const fetchMarketData = async () => {
+    try {
+      const response = await fetch('https://api.coincap.io/v2/assets');
+      const data = await response.json();
+      setCryptoData(data.data);
+    } catch (error) {
+      console.log('Error fetching market data:', error);
+    }
+  };
+
+  //function that calculates the total amount owned for a currency
+  const calculateTotalAmount = (currency) => {
+    const transactionsPerCurrency = transactions[currency];
+    let totalAmount = 0;
+    transactionsPerCurrency.forEach((transaction) => {
+      if (transaction.type === 'purchase') {
+        totalAmount += transaction.amount;
+      } else if (transaction.type === 'sale') {
+        totalAmount -= transaction.amount;
+      }
+    });
+    return totalAmount;
+  };
+
   return (
     <>
       {isLoading ? (
@@ -88,7 +165,7 @@ const Dashboard = () => {
                       className='w-full px-4 py-2 text-base text-gray-700 placeholder-gray-500 border rounded-lg focus:outline-none focus:shadow-outline'
                       id='grid-amount'
                       type='number'
-                      placeholder='50+'
+                      placeholder='50$'
                       min={50}
                       required={true}
                       onChange={(e) => setAmount(e.target.value)}
@@ -104,26 +181,41 @@ const Dashboard = () => {
                     />
                   </div>
                 </div>
+                <div className='border-solid border-2 border-red-500 rounded-lg bg-red-200 p-4 text-center'>
+                  <p className='text-sm text-red-600'>
+                    You need to create a wallet to start trading. <br /> You can
+                    add a minimum of 50$.
+                  </p>
+                </div>
               </form>
             </div>
           ) : (
             <div>
-              <h1 className='text-2xl font-semibold'>Hi, {userData.name}</h1>
-              <p className='text-lg font-medium'>
-                This is your dashboard, here you can see your profile and your
-                statistics.
-              </p>
+              <h1 className='text-2xl font-semibold text-center'>
+                Welcome back {userData.name}!
+              </h1>
               <div className='flex flex-row gap-4 justify-between'>
                 <p className='text-2xl font-medium flex items-center'>
-                  <span className='font-semibold'>Wallet:&nbsp;</span>
-                  {amount ? wallet.startingAmount / 1 + amount : null}€
+                  <span className='font-semibold'>Your assets:&nbsp;</span>
+                  {amount}€
                 </p>
-                <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
-                  Buy crypto
-                </button>
               </div>
             </div>
           )}
+          <div className='border-2 rounded-lg border-gray-100'>
+            <table className='text-center mx-auto w-full'>
+              <thead className='hidden border-b-2 border-gray-200 sm:table-header-group'>
+                <tr className='text-center w-full min-w-full'>
+                  <th className='p-4'>Name</th>
+                  <th className='p-4'>Balance</th>
+                  <th className='p-4'>Market Price</th>
+                </tr>
+              </thead>
+                <tbody>
+                  
+              </tbody>
+            </table>
+          </div>
         </div>
       )}
     </>
