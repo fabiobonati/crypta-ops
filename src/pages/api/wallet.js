6b89820be23982bc8aab2df0929233a9768745ef
fabiobonati <bonatifabio.dev@gmail.com>
2023-05-29 22:16:49 +0200

    🧹 Code cleanup (Wallet API)

--- src/pages/api/wallet.js
+++ src/pages/api/wallet.js
@@ -1,45 +1,7 @@
 import prisma from 'lib/prisma';
 import { getSession } from 'next-auth/react';
 
-async function handleGET(req, res, session) {
-  let assetValueFiat = 0;
-  const wallet = await prisma.User.findUnique({
-    where: {
-      email: session.user.email,
-    },
-    select: {
-      Wallet: {
-        select: {
-          id: true,
-        },
-      },
-    },
-  });
-  let walletId = wallet.Wallet.id;
-  const transactionsPerCurrency = await prisma.Transaction.groupBy({
-    by: ['currency'],
-    select: {
-      currency: true,
-    },
-    _sum: {
-      amount: true,
-    },
-  });
-  const response = await fetch('https://api.coincap.io/v2/assets')
-    .then((response) => response.json())
-    .catch((error) => console.log('error', error));
-  let count = 0;
-  transactionsPerCurrency.map((currency) => {
-    for (let i = 0; i < response.data.length; i++) {
-      if (response.data[i].symbol == currency.currency.toUpperCase()) {
-        assetValueFiat += response.data[i].priceUsd * currency._sum.amount;
-        count += 1;
-      }
-    }
-  });
-  res.json({ totalAssetsFiat: assetValueFiat });
-}
-
+//POST->create new wallet
 async function handlePOST(req, res) {
   const wallet = await prisma.Wallet.create({
     data: {
